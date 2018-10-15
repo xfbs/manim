766e7c10b1c1f6911c743fc8a3d253b5c69039af
Patrick Elsen <pelsen@xfbs.net>
2018-10-16 01:20:49 +0200

    save work

--- cprogramming.py
+++ cprogramming.py
@@ -1,7 +1,6 @@
 from big_ol_pile_of_manim_imports import *
 
 class TitleScene(Scene):
-    #Adding text on the screen
     def construct(self):
         title = TextMobject("\\begin{minipage}{2.3cm}\\begin{flushleft}Introduction to the \phantom{C} Programming Language\\end{flushleft}\\end{minipage}")
         c = TextMobject("\\begin{minipage}{2.3cm}\\begin{flushleft}\phantom{Introduction} \phantom{to} \phantom{the} C \phantom{Programming} \phantom{Language}\\end{flushleft}\\end{minipage}")
@@ -10,26 +9,39 @@ class TitleScene(Scene):
         title.scale(2.5)
         title.shift(3*LEFT)
         c.scale(10)
-        #c.shift(2.82*LEFT+0.85*UP)
          
         self.add(c)
         self.wait(2)
         self.play(ScaleInPlace(c, 0.25))
         self.play(ApplyMethod(c.shift, 2.82*LEFT+0.85*UP))
         self.play(ShowCreation(title))
+        self.wait(2)
         title.add(c)
         self.remove(c)
         self.play(FadeOut(title))
-        #self.play(FadeOut(title))
-        #self.play(FadeToColor(c, SRCERY_GREEN))
-        #self.play(ScaleInPlace(c, 4))
-        #self.add(title)
-        #self.play(ScaleInPlace(title, 2))
-        #self.play(ApplyMethod(title.shift, 3*LEFT))
         self.wait(2)
-        self.add(AppleMac())
+
+class ComputerScene(Scene):
+    def construct(self):
+        computer = Computer()
+        #self.add(computer)
+        self.play(FadeIn(computer))
         self.wait(2)
-        #self.play(Transform(second_line,third_line))
-        #self.wait(2)
-        #second_line.shift(3*DOWN)
-        #self.play(ApplyMethod(my_first_text.shift,3*UP))
+        self.play(ScaleInPlace(computer, 3))
+        self.wait(2)
+
+class Computer(SVGMobject):
+    CONFIG = {
+        "file_name": "apple-01.svg"
+    }
+
+    def __init__(self, **kwargs):
+        SVGMobject.__init__(self, **kwargs)
+        self[0].set_fill("#A6A6AD", 1)
+        self[1].set_fill("#3E3E42", 1) # frame
+        self[2].set_fill("#000000", 0.1)
+        self[3].set_fill("#D2D2D6", 1)
+        self[4].set_fill("#57575E", 1)
+        self[5].set_fill("#A6A6AD", 1)
+        self[6].set_fill("#D2D2D6", 1)
+
